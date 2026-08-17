8407bfd18849d47b8b3927da18ce037a186506e6
めっちゃいいやつのトウヤマ <hozuo0906@gmail.com>
2026-08-17 21:47:05 +0900

    test: validate URL before step capacity

--- tests/sql/phase2-manual-edit-http-test.sql
+++ tests/sql/phase2-manual-edit-http-test.sql
@@ -326,6 +326,22 @@ $$;
 
 do $$
 declare
+  valid_step_id uuid;
+begin
+  valid_step_id := public.append_manual_step(
+    '44444444-4444-4444-8444-444444444444',
+    'action', 'internal host URL', '', 'navigate', '画面', 'https://service_name.example/',
+    null, '{}'::jsonb, '{}'::jsonb
+  );
+  perform public.soft_delete_manual_step(
+    '44444444-4444-4444-8444-444444444444',
+    valid_step_id
+  );
+end;
+$$;
+
+do $$
+declare
   index_no integer;
   rejected boolean := false;
 begin
@@ -433,18 +449,7 @@ do $$
 declare
   candidate text;
   rejected boolean;
-  valid_step_id uuid;
 begin
-  valid_step_id := public.append_manual_step(
-    '44444444-4444-4444-8444-444444444444',
-    'action', 'internal host URL', '', 'navigate', '画面', 'https://service_name.example/',
-    null, '{}'::jsonb, '{}'::jsonb
-  );
-  perform public.soft_delete_manual_step(
-    '44444444-4444-4444-8444-444444444444',
-    valid_step_id
-  );
-
   foreach candidate in array array[
     'https://%',
     'https://[invalid',
